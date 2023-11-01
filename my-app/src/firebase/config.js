3b36095c29768d629d49814fc7eface922b29cbe
Ailin Melinda Fernández <afernandez@udesa.edu.ar>
2023-11-01 08:58:26 -0300

    firebase

--- my-app/src/firebase/config.js
+++ my-app/src/firebase/config.js
@@ -2,7 +2,14 @@ import app from 'firebase/app';
 import firebase from 'firebase';
 
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyBWj-sm9KYtoFd107fscxHNNnQhhsC69zs",
+    authDomain: "proyectoprog-7b763.firebaseapp.com",
+    projectId: "proyectoprog-7b763",
+    storageBucket: "proyectoprog-7b763.appspot.com",
+    messagingSenderId: "1063601823823",
+    appId: "1:1063601823823:web:ec60173d1b80ac214b8a3a"
+  };
 
 
 app.initializeApp(firebaseConfig);
