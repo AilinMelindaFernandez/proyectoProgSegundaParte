ccc262c9766aa0e3eea08613a576eb888dc9b6ec
Tati Lamas <tatilamas@MBP-de-Tati.fibertel.com.ar>
2023-11-01 09:41:18 -0300

    register y login en proceso

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import { auth } from "./src/firebase/config";
+import Register from './src/screens/Register/Register';
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name='Registro' component={Register} options={ { headerShown: false } }/>
+          {/* <Stack.Screen name='Login' component={Login} options={ { headerShown: false } }/> */}
+          {/* <Stack.Screen name='Home' component={Home} options={ { headerShown: false } }/> */}
+
+
+        </Stack.Navigator>
+      </NavigationContainer>
+
   );
 }
 
@@ -15,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
